9713af6a04ff4bdd65a2607c8fd8f0239943ca57
thandinhjs <dinhcongthan03@gmail.com>
2023-10-11 10:09:06 +0700

    init

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import express, { Express } from "express";
+import morgan from "morgan";
+import connectDB from "./config/database";
+import cors from 'cors';
+
+// import swaggerDocs from "./utils/swagger";
+
+const app: Express = express();
+
+dotenv.config();
+
+// Khởi tạo kết nối với cơ sở dữ liệu
+connectDB(process.env.MONGO_URI as string);
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors())
+
+// swaggerDocs(app, 3000);
+
+export const viteNodeApp: Express = app;
