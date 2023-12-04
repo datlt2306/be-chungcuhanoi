50f08b91526a9871d761737d48821266ccf5269e
thandinhjs <dinhcongthan03@gmail.com>
2023-12-04 11:01:30 +0700

    update

--- src/models/category.ts
+++ src/models/category.ts
@@ -3,20 +3,25 @@ import mongoosePaginate from "mongoose-paginate-v2";
 import mongooseDelete from "mongoose-delete";
 import { ICategory } from "../interfaces/category";
 
-const categorySchema = new mongoose.Schema<ICategory>({
-  category_name: {
-    type: String,
-    minLength: 3,
-    maxlength: 50,
+const categorySchema = new mongoose.Schema<ICategory>(
+  {
+    category_name: {
+      type: String,
+      minLength: 3,
+      maxlength: 50,
+    },
+    category_description: {
+      type: String,
+      minlength: 3,
+      maxlength: 500,
+    },
   },
-  category_description:{
-    type: String,
-    minlength:3,
-    maxlength:200
-  },
-},
-  { timestamps: true, versionKey: false });
+  { timestamps: true, versionKey: false }
+);
 
 categorySchema.plugin(mongoosePaginate);
-categorySchema.plugin(mongooseDelete, { overrideMethods: 'all',deletedAt : true  });
-export default mongoose.model("Category", categorySchema)
+categorySchema.plugin(mongooseDelete, {
+  overrideMethods: "all",
+  deletedAt: true,
+});
+export default mongoose.model("Category", categorySchema);
